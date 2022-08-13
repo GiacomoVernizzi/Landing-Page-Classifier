e438992553b4d7acdabb57651f697df89f463363
Giacomo Vernizzi, MSc <76483706+GiacomoVernizzi@users.noreply.github.com>
2022-08-13 16:12:08 +0100

    Create app.py

---
+++ app.py
@@ -0,0 +1,34 @@
+import cv2
+import numpy as np
+import streamlit as st
+import tensorflow as tf
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.applications.mobilenet_v2 import MobileNetV2,preprocess_input as mobilenet_v2_preprocess_input
+
+model = tf.keras.models.load_model('/Users/giacomo/Desktop/LP Classifier Web Page/LP_classifier.h5')
+### load file
+uploaded_file = st.file_uploader("Choose a image file", type=['png', 'jpg', 'jpeg'])
+
+
+if uploaded_file is not None:
+    # Convert the file to an opencv image.
+    file_bytes = np.asarray(bytearray(uploaded_file.read()), dtype=np.uint8)
+    opencv_image = cv2.imdecode(file_bytes, 1)
+    opencv_image = cv2.cvtColor(opencv_image, cv2.COLOR_BGR2RGB)
+    resized = cv2.resize(opencv_image,(256,256))
+    # Display image:
+    st.image(opencv_image, channels="RGB")
+
+    resized = mobilenet_v2_preprocess_input(resized)
+    img_reshape = resized[np.newaxis,...]
+
+    Genrate_pred = st.button("Generate Prediction")    
+    if Genrate_pred:
+        prediction = model.predict(img_reshape)
+        st.title(prediction)
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Security
