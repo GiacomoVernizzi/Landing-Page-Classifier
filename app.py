587f4a6dc5d5a056a55171eef0e1609fe35eff5f
Giacomo Vernizzi, MSc <76483706+GiacomoVernizzi@users.noreply.github.com>
2022-10-09 17:00:06 +0100

    Update app.py

--- app.py
+++ app.py
@@ -7,8 +7,8 @@ from tensorflow.keras.applications.mobilenet_v2 import MobileNetV2,preprocess_in
 
 st.title('Landing Page Classfier')
 
-st.subheader('Unsure whether your landing page will convert or not? This is a CNN classifier trained on 100 good\ 
-and 100 bad Ad landing pages. Just screeshot your landing page and drop it into the box below to generate an instant prediction!')
+st.subheader('Unsure whether your landing page will convert or not? This is a CNN classifier trained on 100 good and 100 bad Ad landing pages. \
+Just screeshot your landing page and drop it into the box below to generate an instant prediction!')
 
 model = tf.keras.models.load_model('LP_classifier.h5')
 ### load file
